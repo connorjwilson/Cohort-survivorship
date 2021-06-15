741e11fd10be9f684f05b05631de7c4fb2498a7a
Connor Wilson <connorwilson@Connors-MacBook-Pro.local>
2021-06-15 10:33:29 -0700

    Finish slope example

--- Scripts/slope-calculation.R
+++ Scripts/slope-calculation.R
@@ -1,15 +1,28 @@
+library(divDyn)
+data(keys)
+data(stages)
+
+data <- read.csv("Data/pbdb_na.csv", skip = 20, header = TRUE)
+
 neg_stages <- -1 * stages$mid
 
 ## North America
 na_cohort <- surv[82:95,82:95]
 
-na_slopes <- vector()
-for (i in 1:14){
-  na_slopes[i] <- lm(log(na_cohort[i:max(which(na_cohort[,i]>0)),i])
-                     ~ neg_stages[81+i:81+max(which(na_cohort[,i]>0))])
+na_fits <- list()
+for (i in 1:14) {
+  na_fits[[i]] <- lm(log(na_cohort[i:max(which(na_cohort[,i]>0)),i])
+                     ~ neg_stages[(81+i):(81+max(which(na_cohort[,i]>0)))])
 }
 
-na_fit1 <- lm(na_cohort[,1] ~ stages$mid[81+i:81+max(which(na_cohort[,i]>0))])
-na_fit2 <- lm(log(na_cohort[1:max(which(na_cohort[,i]>0)),1]) ~ neg_stages[82:87])
+na_slopes <- vector()
+for (i in 1:14) {
+  na_slopes[i] <- na_fits[[i]]$coefficients[1]
+}
 
-neg_stages[81+1 : 81+max(which(na_cohort[,1]>0))]
+na_result <- lm (na_slopes ~ neg_stages[82:95])
+plot(neg_stages[82:95], na_slopes)
+abline(na_result)
+
+plot(stages[82:95,9], na_slopes)
+plot(neg_stages[82:95], stages[82:95,9])
