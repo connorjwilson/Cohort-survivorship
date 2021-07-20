c5080b4c6424347a276f6d72e9a9bf3e1dbb5b15
Connor Wilson <connorwilson@Connors-MacBook-Pro.local>
2021-07-20 12:46:35 -0700

    Simulations update

---
+++ Scripts/Untitled.R
@@ -0,0 +1,16 @@
+########################
+# parameter-fitting.R
+########################
+
+slopes <- c(na_slopes)
+times <- c(neg_stages[82:93])
+
+df <- as.data.frame(cbind(slopes, times))
+
+plot(times, slopes)
+fitting <- lm(slopes ~ times+ I(times^2)+I(times^3))
+
+x <- with(df, seq(min(times), max(times), length.out=2000))
+y <- predict(fitting, newdata=data.frame(times=x))
+
+lines(x,y)
